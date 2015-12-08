917bcc8d94e9459afea8ece46bc502ee5c239be5
trolizmaslom <trolizmaslom@gmail.com>
2015-12-08 16:18:48 +0200

    base

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+/* plagins */
+
+//= partials/plagins/jquery-2.1.4.min.js
+
+//= partials/plagins/maskInput.js
+//= partials/plagins/slick.js
+//= partials/plagins/jquery.validate.min.js
+//= partials/plagins/jquery.fancybox.js
+
+/* /plagins */
+
+//= partials/basic_scripts.js
+
+//= partials/validate_script.js
+
+//= partials/develop_1.js
+//= partials/develop_2.js
+//= partials/develop_3.js
+//= partials/develop_4.js
+//= partials/develop_5.js
---
+++ src/js/partials/basic_scripts.js
@@ -0,0 +1,90 @@
+/* input only Number  */
+function inputNumber(block) {
+    $('input', block).keypress(function(e) {
+        if (e.which >= 47 && e.which <= 57 ){}
+        else return false;
+    });
+
+    $('input', block).keyup(function() {
+        $inputNum = $(this);
+        if ($inputNum.val == '' || $inputNum.val() == 0) {
+            $inputNum.val('');
+        }
+    });
+}
+
+/* scrollUp */
+function scrollUp(block,targetBlock) {
+
+    $(block).click(function(e){
+        var target = $(targetBlock).offset().top;
+
+        $(scroller).animate({scrollTop:target},800);
+        return false;
+
+        e.preventDefault();
+    });
+}
+
+function initScrollpane(){
+    $('.scroll-pane').jScrollPane();
+}
+
+
+function oneHeightItems(){
+
+    function oneHeight(block){
+        var height=0;
+        block.removeAttr('style');
+        block.each(function(){
+            if($(this).height()>height){
+                height=$(this).height();
+            }
+        });
+        block.css('height', height);
+    }
+
+    oneHeight($('.oneHeight'));
+}
+
+function animationBlock(item){
+
+    $(window).scroll(function(){
+        checkForAnimate();
+    });
+
+    function checkForAnimate(){
+        var bottomCheck = $(window).height()+$(window).scrollTop();
+        var windowTop = $(window).scrollTop()+($(window).height()/1.5);
+        item.each(function(){
+           if(windowTop>$(this).offset().top || bottomCheck > $('body').height()*0.98){
+
+              var itemSect = $(this);
+              var point = 0;
+              itemSect.find('.animate-it').addClass('animated');
+
+              var timer = setInterval(function(){
+                 itemSect.find('.animate-delay').eq(point).addClass('animated');
+                 point++;
+                 if(itemSect.find('.animate-delay').length == point){
+                     clearInterval(timer);
+                 }
+              },200);
+
+
+           }
+        });
+    }
+    checkForAnimate();
+}
+
+/* DOCUMENT READY  */
+$(document).ready(function() {
+
+    oneHeightItems();
+});
+
+$(window).resize(function() {
+
+    $('.footer_placeholder').height($('.footer').outerHeight());
+});
