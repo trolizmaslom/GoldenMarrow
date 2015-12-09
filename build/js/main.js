cbc6768c7c5e22597f61891e8df14c25a1dabbc9
Radiksman <radion91@gmail.com>
2015-12-09 17:18:40 +0200

    add some shit

--- build/js/main.js
+++ build/js/main.js
@@ -5265,8 +5265,17 @@ function sectionSlider(){
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        draggable:false,
+        centerPadding:'30px'
     });
+    $('.section-9-slider').on('beforeChange', function(){
+        $('.item-slide-hidden').fadeOut();
+    });
+    $('.section-9-slider').on('afterChange', function(){
+        $('.item-slide.slick-center').find('.item-slide-hidden').fadeIn();
+    });
+
 }
 
 $(document).ready(function(){
--- src/js/partials/develop_4.js
+++ src/js/partials/develop_4.js
@@ -3,8 +3,17 @@ function sectionSlider(){
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        draggable:false,
+        centerPadding:'30px'
     });
+    $('.section-9-slider').on('beforeChange', function(){
+        $('.item-slide-hidden').fadeOut();
+    });
+    $('.section-9-slider').on('afterChange', function(){
+        $('.item-slide.slick-center').find('.item-slide-hidden').fadeIn();
+    });
+
 }
 
 $(document).ready(function(){
