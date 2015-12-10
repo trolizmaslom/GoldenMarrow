2e2407b48f74dbe0df3ce65eff6d9158d64a9eb0
Radiksman <radion91@gmail.com>
2015-12-10 09:22:20 +0200

    add some shit php

--- src/ajax.php
+++ src/ajax.php
@@ -1,1 +1,44 @@
-true
+<?php
+    $subject = 'Заявка с сайта';
+    $mess = '';
+    $mess .= '<hr>';
+    if(isset($_POST['info'])) {
+        $subject = $_POST['info'];
+    }
+    if(isset($_POST['contact_name'])) {
+        $name = substr(htmlspecialchars(trim($_POST['contact_name'])), 0, 100);
+        $mess .= '<b>Имя:</b>' . $name . '<br>';
+    }
+    if(isset($_POST['phone'])) {
+        $tel = substr(htmlspecialchars(trim($_POST['phone'])), 0, 100);
+        $mess .= '<b>Телефон:</b>' . $tel . '<br>';
+    }
+    if(isset($_POST['userEmail'])) {
+        $mail = substr(htmlspecialchars(trim($_POST['userEmail'])), 0, 100);
+        $mess .= '<b>Почта:</b>' . $mail . '<br>';
+    }
+    if(isset($_POST['contact_textarea'])) {
+        $mail = substr(htmlspecialchars(trim($_POST['contact_textarea'])), 0, 100);
+        $mess .= '<b>Вопрос:</b>' . $mail . '<br>';
+    }
+    $mess .= '<hr>';
+    // подключаем файл класса для отправки почты
+    require 'class.phpmailer.php';
+
+    $mail = new PHPMailer();
+    $mail->AddAddress('mail@msk-uber.ru','');   // кому - адрес, Имя
+    $mail->IsHTML(true);                        // выставляем формат письма HTML
+    $mail->Subject = $subject; // тема письма
+    $mail->CharSet = "UTF-8";                   // кодировка
+    $mail->Body = $mess;
+    if(isset($_FILES['file'])) {
+            if($_FILES['file']['error'] == 0){
+            $mail->AddAttachment($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+        }
+    }
+    // отправляем наше письмо
+    if (!$mail->Send()){
+        die ('Mailer Error: ' . $mail->ErrorInfo);
+    }else{
+        echo 'true';
+    }?>
